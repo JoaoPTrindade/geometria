6afe7a30529ef61694ddbc071e731101b576f8be
ArthBM <146386068+ArthBM@users.noreply.github.com>
2024-02-23 17:17:42 -0300

    Add files via upload

--- classe.js
+++ classe.js
@@ -1,23 +1,36 @@
-class Geometria{
+class Geometria {
+
     constructor(area,altura,base,raio){
+
         this.area = area
         this.altura = altura
         this.base = base
         this.raio = raio
-    }
-    calculaPrisma(){ // Arthur
 
     }
-    calculaPiramide(){ // Eduardo
 
+
+    calculaPrisma(){
+    // Área * Base * Altura
+        let v = 0
+
+        v = (this.area * this.base) * this.altura
+
+        return v
+        
     }
-    calculaCilindro(){ // Laura
 
-    }
-    calculaCone(){ // Eduardo
+    
+    calculaEsfera(){
+    // (4/3) pir³
+        let v = 0
 
-    }
-    calculaEsfera(){ // Arthur
+        v = 1,333333333333333 * (3.14159265359 *(this.raio ^ 3))
 
+        return v
+    
     }
 }
+
+
+
