3b40ca259a496e204cf1f4139110550d64261ea4
João Paulo <joao_pt_costa@estudante.sesisenai.org.br>
2024-02-23 16:45:18 -0300

    Protótipo

---
+++ classe.js
@@ -0,0 +1,23 @@
+class Geometria{
+    constructor(area,altura,base,raio){
+        this.area = area
+        this.altura = altura
+        this.base = base
+        this.raio = raio
+    }
+    calculaPrisma(){ // Arthur
+
+    }
+    calculaPiramide(){ // Eduardo
+
+    }
+    calculaCilindro(){ // Laura
+
+    }
+    calculaCone(){ // Eduardo
+
+    }
+    calculaEsfera(){ // Arthur
+
+    }
+}
